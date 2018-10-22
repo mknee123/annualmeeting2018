362ac069b448d03603e7620c1d9fbb7d7a3bf6b7
MurKat <mknee123@gmail.com>
2018-10-22 14:09:33 -0400

    Added in jQuery code to make subnav appear on click of +. Sub nav hides when user clicks anywhere else or on another sub-nav. Added navigation links to growing economy and improving competitiveness pages.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,23 @@
+/*
 $('.caret').click(function(e) {
   e.preventDefault();
   $('.sub-nav').not($(this).next('.sub-nav')).slideUp();
   $(this).next('.sub-nav').slideToggle();
+});
+
+*/
+
+$(".caret").click(function(event){
+    event.preventDefault();
+      $('.sub-nav').not($(this).next('.sub-nav')).slideUp();
+			$(this).next('.sub-nav').slideToggle();
+			event.stopPropagation();
+});
+/*
+$('.sub-nav').click(function(event){
+	event.stopPropagation();
+			});
+      */
+$('html').click(function() {
+		$('.sub-nav').slideUp();
 });
